102c32cfcfc2ab46fb283fa62b7a7a010c0edb2f
SnowzTail <i@snowztail.com>
2018-11-02 14:00:18 +0000

    Iterative hardthresholding fix
    no need for projection onto subspace

--- iterative_hardthresholding.m
+++ iterative_hardthresholding.m
@@ -3,14 +3,20 @@ function [xIterativeHardthresholding] = iterative_hardthresholding(sparseCardina
 xIterativeHardthresholding = zeros(size(a, 2), 1);
 doTerminate = 0;
 normalizedError = 1;
-
+yResidue = y;
 while (~ doTerminate)
     normalizedErrorLast = normalizedError;
-    sparseSupport = hard_threshold(xIterativeHardthresholding + a' * (y - a * xIterativeHardthresholding), sparseCardinality);
-    aSparseIter = a(:, sparseSupport);
-    xSparseIter = aSparseIter \ y;
+%     sparseSupport = hard_threshold(xIterativeHardthresholding + a' * (y - a * xIterativeHardthresholding), sparseCardinality);
+%     aSparseIter = a(:, sparseSupport);
+%     xSparseIter = aSparseIter \ y;
 %     yResidue = y - aSparseIter * xSparseIter;
-    xIterativeHardthresholding(sparseSupport) = xSparseIter;
+%     xIterativeHardthresholding(sparseSupport) = xSparseIter;
+    
+
+    xPreSparse = xIterativeHardthresholding + a' * yResidue;
+    sparseSupport = hard_threshold(xIterativeHardthresholding + a' * yResidue, sparseCardinality);
+    xIterativeHardthresholding = zeros(size(a, 2), 1);
+    xIterativeHardthresholding(sparseSupport) = xPreSparse(sparseSupport);
     yResidue = y - a * xIterativeHardthresholding;
     % terminate conditions
     normalizedError = norm(yResidue) / norm(y);
